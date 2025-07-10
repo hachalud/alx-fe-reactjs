2b8015cb413872239077909b5db0ecf21cf2a9a2
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-07-10 22:22:12 +0300

    multiple component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-
+import MainContent from './components/MainContent.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

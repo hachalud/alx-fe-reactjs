32b5856dc1431802236204b96ec04b4a41d5b123
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-07-13 13:39:53 +0300

    last commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
-const UserProfile = ({name, age, bio}) => {
+const UserProfile = (props) => {
   return (
     <div>
-      <h2>Name: {name}</h2>
-      <p>Age: {age}</p>
-      <p>Bio: {bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };

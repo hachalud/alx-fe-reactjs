75d7c036ded262d87e4360e4ad20b54e9b5b6ab6
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-07-20 08:29:02 +0300

    inline CSS

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "lightgray", textAlign: "center", padding: "10px" }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer

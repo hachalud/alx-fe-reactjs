17f7b159e69a8df37729c74a2569c349949c3308
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-07-20 08:52:27 +0300

    state

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import MainContent from './components/MainContent.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,6 +17,7 @@ function App() {
       <WelcomeMessage />
       <MainContent />
       <Footer />
+      <Counter />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <div>
         <a href="https://vite.dev" target="_blank">

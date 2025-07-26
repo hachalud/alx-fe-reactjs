9aabcc2393e9d41422a7816ca4453c5580a90792
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-07-26 11:34:00 +0300

    Detaels

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,35 @@
 import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
+  recipes: [
+    {
+      id: 1,
+      title: "Pasta",
+      description: "Delicious Italian pasta with tomato sauce.",
+    },
+    {
+      id: 2,
+      title: "Salad",
+      description: "Fresh mixed green salad with vinaigrette.",
+    },
+  ],
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
   setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id
+          ? { ...recipe, ...updatedRecipe }
+          : recipe
+      ),
+    })),
 }));
 
 export default useRecipeStore;
